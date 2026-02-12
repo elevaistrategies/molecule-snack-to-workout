917d757f1c6d281d1be4d44379493f6d6e70d5bd
ElevAI Strategies <227914380+elevaistrategies@users.noreply.github.com>
2026-02-12 12:07:18 -0600

    Create data.snack.js

---
+++ js/data.snack.js
@@ -0,0 +1,7 @@
+const SNACKS = [
+  { name:"Glazed Donut", cal:260 },
+  { name:"Big Mac", cal:550 },
+  { name:"Large Fries", cal:510 },
+  { name:"Protein Bar", cal:220 },
+  { name:"3 Slices Pizza", cal:840 }
+];
