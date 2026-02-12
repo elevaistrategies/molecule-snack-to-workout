9517716a3cad4565e282afe7699571d3f060e60b
ElevAI Strategies <227914380+elevaistrategies@users.noreply.github.com>
2026-02-12 13:34:28 -0600

    Update data.snacks.js

--- js/data.snacks.js
+++ js/data.snacks.js
@@ -1,11 +1,11 @@
 // Snack presets (expand freely). Keep it light, keep it funny.
 window.SNACKS = [
-  { id:"donut",   name:"Glazed Donut 🍩", cal:260 },
+  { id:"chips",   name:"Big Bag of Chips 🥔", cal:480 },
   { id:"bigmac",  name:"Big Mac 🍔", cal:550 },
+  { id:"boba",    name:"Bubble Tea 🧋", cal:380 },
+  { id:"donut",   name:"Glazed Donut 🍩", cal:260 },
+  { id:"icecream",name:"Ice Cream Pint 🍨", cal:1000 },
   { id:"fries",   name:"Large Fries 🍟", cal:510 },
   { id:"pizza3",  name:"3 Slices Pizza 🍕", cal:840 },
-  { id:"boba",    name:"Bubble Tea 🧋", cal:380 },
   { id:"bar",     name:"Protein Bar 💪", cal:220 },
-  { id:"chips",   name:"Big Bag of Chips 🥔", cal:480 },
-  { id:"icecream",name:"Ice Cream Pint 🍨", cal:1000 },
 ];
